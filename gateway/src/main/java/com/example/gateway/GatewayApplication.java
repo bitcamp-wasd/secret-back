7b7040bc18a341d2ecc6da16fd67053ad06fb97b
maruduke <abab1117@naver.com>
2024-06-17 00:32:38 +0900

    MSA Config (eureka, config)

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,13 +1,27 @@
 package com.example.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApplication implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+
+    @Value("${test.code}")
+    String test;
+    @Value("${application.test}")
+    String root;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(test);
+    }
 }
