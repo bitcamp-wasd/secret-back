02ce5bacdb3ac5fb384bbecc03abec0696619485
maruduke <abab1117@naver.com>
2024-07-01 09:13:04 +0900

    gateway, user 프로젝트 redis 설정 변경

--- user/src/main/java/com/example/user/config/RedisConfig.java
+++ user/src/main/java/com/example/user/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
@@ -26,6 +27,12 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(false);
+
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
--- user/src/main/java/com/example/user/service/RedisService.java
+++ user/src/main/java/com/example/user/service/RedisService.java
@@ -2,12 +2,14 @@ package com.example.user.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
+@Log4j2
 public class RedisService {
 
     private final RedisTemplate<String, Object> redisTemplate;
@@ -19,19 +21,19 @@ public class RedisService {
     public void set(String key, String value, long expirationTime) {
         redisTemplate.opsForValue().set(key, value, expirationTime, TimeUnit.SECONDS);
         // 로그 추가
-        System.out.println("레디스에 저장: " + key + " -> " + value + " (" + expirationTime + "초)");
+        log.info("레디스에 저장: " + key + " -> " + value + " (" + expirationTime + "초)");
     }
 
     public String get(String key) {
         String value = (String) redisTemplate.opsForValue().get(key);
         // 로그 추가
-        System.out.println("레디스에서 가져옴: " + key + " -> " + value);
+        log.info("레디스에서 가져옴: " + key + " -> " + value);
         return value;
     }
 
     public void delete(String key) {
         redisTemplate.delete(key);
-        System.out.println("레디스에서 삭제: " + key);
+        log.info("레디스에서 삭제: " + key);
     }
 
     public void setTokenData(String token, Long userId, String role, long expirationTime) {
