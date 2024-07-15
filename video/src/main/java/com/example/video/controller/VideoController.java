d3aa311530bad6589aede49f774d43be38230a83
maruduke <abab1117@naver.com>
2024-07-15 13:20:55 +0900

    VideoId로 PostId 값 가져오기

--- video/src/main/java/com/example/video/controller/VideoController.java
+++ video/src/main/java/com/example/video/controller/VideoController.java
@@ -1,6 +1,8 @@
 package com.example.video.controller;
 
+import com.example.video.dto.post.response.PostIdDto;
 import com.example.video.dto.post.response.VideoResponseDto;
+import com.example.video.entity.Video;
 import com.example.video.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,20 @@ public class VideoController {
 
     private final VideoService videoService;
 
+    /**
+     * 비디오 상세 정보 가져오기
+     * @param videoId
+     * @return
+     */
     @GetMapping("watch")
     public VideoResponseDto getVideo(@RequestParam("id") Long videoId) {
         VideoResponseDto videoResponseDto = videoService.getVideo(videoId);
         return videoResponseDto;
+    }
+
+    @GetMapping("info")
+    public PostIdDto postId(@RequestParam("videoId") Long videoId) {
+        Long postId = videoService.getPostId(videoId);
+        return new PostIdDto(postId);
     }
 }
